d29057bc4b06db8481099649af3947da48a1d8f0
Jesus Hernandez <mrjesus360@gmail.com>
2025-11-11 01:29:20 -0400

    Refactor SQL queries to include Segment grouping

--- sql/customer-insights-segmentation-project.sql
+++ sql/customer-insights-segmentation-project.sql
@@ -10,32 +10,32 @@ ORDER BY 'Total Sales', 'Total Profit'; -- Finally, the results are ordered by T
 
 -- [2] Who are the top 10 customers contributing most to revenue?
 
-SELECT `Customer Name`, ROUND(SUM(Sales), 2) AS `Total Revenue` -- Select the customer name and calculate their total revenue using the sum of sales.
+SELECT Segment, (`Customer Name`), ROUND(SUM(Sales), 2) AS `Total Revenue` -- Select the customer name and calculate their total revenue using the sum of sales.
 -- , ROUND(SUM(Sales) / (SELECT SUM(SALES) FROM projects.superstore) * 100, 2) AS `% of Total Revenue` -- Optionally, compute each customer's percentage contribution to overall sales.
 FROM projects.superstore
-GROUP BY `Customer Name`
+GROUP BY `Customer Name`, Segment
 ORDER BY `Total Revenue` DESC
-LIMIT 10; -- Limit results to the top 10 highest-spending customers.
+LIMIT 10; -- Limit the results to the top 10 highest spending customers.
 
--- [3] What's the average profit margin and discount by segment or region?
+-- [3] What’s the average profit margin and discount by segment or region?
 
 SELECT Segment, ROUND(AVG(Profit/Sales * 100), 2) AS `Average Profit Margin %`,
-ROUND(AVG(Discount * 100), 2) AS `Average Discount %` -- Multiplied discount by 100 because it's a percentage.
+ROUND(AVG(Discount * 100), 2) AS `Average Discount %` -- Multiplied discount by 100 because it's a percentage
 FROM projects.superstore
 GROUP BY Segment
 ORDER BY `Average Profit Margin %` DESC;
 
 -- [4] How many unique customers did we serve per month or quarter?
 
-SELECT DATE_FORMAT(STR_TO_DATE(`Order Date`, '%m%d%Y'), '%Y-%m') AS `Month`, -- Format the Date because it was formatted as STR.
+SELECT Segment, DATE_FORMAT(STR_TO_DATE(`Order Date`, '%m/%d/%Y'), '%Y-%m') AS `Month`, -- Format the Date because it was formatted as STR.
 COUNT(DISTINCT(`Customer Name`)) AS `Unique Customers` -- Count each Unique customer by month.
 FROM projects.superstore
-GROUP BY `Month`
+GROUP BY `Month`, Segment
 ORDER BY `Month` ASC; -- Ordered by oldest year to most recent, and first to last month of each year.
 
 -- [5] Do customers in some regions or segments buy more frequently or in larger quantities?
 
-SELECT Region, Segment, COUNT(`Order ID`) AS `Amount of Orders` -- Select the amount of orders per Region and Segment
+SELECT Segment, COUNT(`Order ID`) AS `Amount of Orders` -- Select the amount of orders per Region and Segment
 FROM projects.superstore
-GROUP BY Region, Segment
+GROUP BY Segment
 ORDER BY `Amount of Orders` DESC;
